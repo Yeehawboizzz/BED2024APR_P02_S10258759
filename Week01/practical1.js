ee7d90ae2e3527701e087c942e6b8c7b16ffe905
Yeehawboizzz <s10258759@connect.np.edu.sg>
2024-04-29 10:45:51 +0800

    Completed Week01 Practical

---
+++ Week01/practical1.js
@@ -0,0 +1,29 @@
+const path = require('node:path');
+
+const notes = '/users/joe/notes.txt';
+
+path.dirname(notes);
+path.basename(notes);
+path.extname(notes);
+
+
+const fs = require('node:fs');
+fs.readFile('/Users/joe/test.txt','utf8',(err,data) => {
+    if(err){
+        console.error(err);
+        return;
+    }
+    console.log(data);
+});
+
+const content = 'Some content!';
+fs.writeFile('/Users/joe/test.txt',content,err=>{
+    if(err){
+        console.error(err);
+    } else{
+        //file written successfully
+    }
+});
+
+const chalk = require('chalk');
+console.log(chalk.yellow('hi!'));
